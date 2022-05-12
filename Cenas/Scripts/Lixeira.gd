cae3e511129e8b77646bb983eef81c94d473ea83
Danilito8911 <daniloplusek@gmail.com>
2022-05-12 20:01:54 -0300

    Pi-Jogos-logicos

---
+++ Cenas/Scripts/Lixeira.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+# Variáveis
+
+# Chamado quando o nó (godot) entra na árvore de cena pela primeira vez.
+func _ready():
+	pass
+
+# Chamado a cada frame. 'delta' é o tempo que passou desde a última frame.
+func _process(delta):
+	pass
+
+func _on_Entrada_body_entered(body):
+	if body.is_in_group("dado"):
+		# Deleta o dado que entrar em contato com este nó (self)
+		body.queue_free()
