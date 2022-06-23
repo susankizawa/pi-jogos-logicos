dc4955223d1f5837f7dcc720e3c170c4b0041875
Fenisu <91688026+susankizawa@users.noreply.github.com>
2022-06-23 17:13:27 -0300

    Coisas visuais
    Barras adicionadas na saída + as propriedades não exigidas ficam cinzas

--- Cenas/Scripts/Saida.gd
+++ Cenas/Scripts/Saida.gd
@@ -22,10 +22,26 @@ var passado = false
 
 # Nós
 onready var game = get_tree().get_root().get_node("Main/Game")
+onready var propriedade1_img = get_node("Propriedade1")
+onready var propriedade2_img = get_node("Propriedade2")
+onready var propriedade3_img = get_node("Propriedade3")
+onready var quant_min_barra = get_node("QuantidadeMin")
+onready var quant_min_text = get_node("QuantidadeMinNum")
+onready var precisao_barra = get_node("Precisao")
+onready var precisao_text = get_node("PrecisaoNum")
 
 # Chamado quando o nó (godot) entra na árvore de cena pela primeira vez.
 func _ready():
-	pass
+	quant_min_text.set_text("%d" % num_min_dados)
+	quant_min_barra.max_value = num_min_dados
+	
+	if !exige_vermelho:
+		propriedade1_img.modulate = Color(0.5,0.5,0.5)
+	if !exige_azul:
+		propriedade2_img.modulate = Color(0.5,0.5,0.5)
+	if !exige_verde:
+		propriedade3_img.modulate = Color(0.5,0.5,0.5)
+	
 
 # Chamado a cada frame. 'delta' é o tempo que passou desde a última frame.
 func _process(delta):
@@ -33,6 +49,9 @@ func _process(delta):
 		if !passado:
 			game.saidas_passadas += 1
 			passado = true
+	
+	quant_min_barra.value = dados.size()
+	precisao_barra.value = precisao
 
 func _on_Entrada_body_entered(body):
 	if body.is_in_group("dado"):
